9c5bd0477fb11a29334f621ffebfa71af26daead
Mseong <mseong123@hotmail.com>
2024-07-26 14:23:03 +0800

    encryption phrase done

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -12,6 +12,7 @@ import Alert from 'react-bootstrap/Alert';
 import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 export function Account ({encryptionPhrase,setEncryptionPhrase}) {
     const packageId = useNetworkVariable('packageId');
@@ -45,24 +46,30 @@ export function Account ({encryptionPhrase,setEncryptionPhrase}) {
               {!encryptionPhrase? (
                 <>
                   <Card.Title>Please enter Encryption Phrase</Card.Title>
+                  <hr></hr>
                   <Card.Text>
                     Encryption Phrase is used to encrypt your files on the blockchain to ensure confidentiality and security of your data.
                     Please use a memorable and unique passphrase <span style={{color:"orange"}}>(your wallet's private key is a good example)</span>
                   </Card.Text>
                   
                 </>):<Card.Title>Encryption phrase: {encryptionPhrase}</Card.Title>}
-                <Form>
-                    <Form.Group controlId="passphrase" className="mb-3">
-                      <Form.Control 
-                          type="text"
-                          size="sm"
-                          htmlSize={30}
-                          placeholder="Encryption Passphrase"
-                      />
-                    </Form.Group>
-                    <Button type="button" onClick={(e)=>{e.preventDefault();
-                      setEncryptionPhrase(document.getElementById("passphrase").value);
-                      document.getElementById("passphrase").value = ""}}>{encryptionPhrase? "Change":"Enter"}</Button>
+                <Form onSubmit={(e)=>{e.preventDefault();
+                        setEncryptionPhrase(document.getElementById("passphrase").value);
+                        document.getElementById("passphrase").value = ""}}>
+                    <InputGroup>
+                      
+                        <Form.Control
+                            id="passphrase"
+                            type="text"
+                            size="sm"
+                            placeholder="Encryption Passphrase (max size 30)"
+                            maxLength={30}
+                        />
+                     
+                      <Button type="submit">
+                          {encryptionPhrase? "Change":"Enter"}
+                      </Button>
+                    </InputGroup>
                 </Form>
                 
               </Card.Body>
